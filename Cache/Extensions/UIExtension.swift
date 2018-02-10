f97aaf53db0012f902cbaf11cfbbc26a92c082d3
Jeffrey Santana <santana@jefffreys-mini.attlocal.net>
2018-02-10 14:49:00 -0500

    Main UI screen ready

---
+++ Cache/Extensions/UIExtension.swift
@@ -0,0 +1,33 @@
+//
+//  UIExtension.swift
+//  Cache
+//
+//  Created by Jeffrey Santana on 2/10/18.
+//  Copyright © 2018 Jefffrey Santana. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class DesignableView: UIView {
+}
+
+@IBDesignable
+class DesignableButton: UIButton {
+}
+
+@IBDesignable
+class DesignableLabel: UILabel {
+}
+
+extension UIView {
+	@IBInspectable
+	var cornerRadius: CGFloat {
+		get {
+			return layer.cornerRadius
+		}
+		set {
+			layer.cornerRadius = newValue
+		}
+	}
+}
---
+++ Cache/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Cache
+//
+//  Created by Jeffrey Santana on 2/10/18.
+//  Copyright © 2018 Jefffrey Santana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+	
+	@IBOutlet weak var cashLbl: UILabel!
+	
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	@IBAction func profileBtnPressed(_ sender: Any) {
+	}
+	
+	@IBAction func historyBtnPressed(_ sender: Any) {
+	}
+	
+	@IBAction func cashInputBtnPressed(_ sender: Any) {
+	}
+	
+	
+}
+
