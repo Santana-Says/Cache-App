1bb7796f8b4c035b5960bbdf6e23a3e6e7c1261b
Jeffrey Santana <santana@jefffreys-mini.attlocal.net>
2018-02-10 17:19:55 -0500

    Number pad is functioning, updates main label

--- Cache/ViewController.swift
+++ Cache/ViewController.swift
@@ -10,23 +10,75 @@ import UIKit
 
 class ViewController: UIViewController {
 	
+	//MARK: - IBOutlets
+	@IBOutlet weak var dollarLbl: UILabel!
 	@IBOutlet weak var cashLbl: UILabel!
 	
+	//MARK: - Properties
+	let DECIMAL_TAG = 10
+	let DELETE_TAG = 11
+	let MAX_INPUT = 5
+	let MAX_AMOUNT = 20000
+	let MAX_COUNT = 8
+	
+	var amountString: String!
+	var amount: NSNumber = 0
 
+	//MARK: - View Cycle
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		
 	}
-
+	
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		amountString = ""
+	}
+	
+	//MARK: - Helper Methods
+	func showAmount(tag: String) -> String {
+		amount = NSNumber(value: Float(amountString)!)
+		
+		let currencyFormatter = NumberFormatter()
+		currencyFormatter.groupingSeparator = ","
+		currencyFormatter.numberStyle = .decimal
+		
+		if Int(amount) < MAX_AMOUNT && amountString.count <= MAX_COUNT {
+			return currencyFormatter.string(from: amount)!
+		} else {
+			amountString.removeLast(1)
+			amount = NSNumber(value: Float(amountString)!)
+			return currencyFormatter.string(from: amount)!
+		}
+	}
+	
+	//MARK: - IBActions
 	@IBAction func profileBtnPressed(_ sender: Any) {
 	}
 	
 	@IBAction func historyBtnPressed(_ sender: Any) {
 	}
 	
-	@IBAction func cashInputBtnPressed(_ sender: Any) {
+	@IBAction func cashInputBtnPressed(_ sender: UIButton) {
+		if sender.tag < DECIMAL_TAG {
+			amountString.append("\(sender.tag)")
+		} else if sender.tag == DECIMAL_TAG {
+			amountString.append(".")
+		} else {
+			amountString.removeLast(1)
+			guard !amountString.isEmpty else {
+				cashLbl.text = "0"
+				return
+			}
+		}
+		cashLbl.text = showAmount(tag: amountString)
 	}
 	
+	@IBAction func requestBtnPressed(_ sender: Any) {
+	}
+	
+	@IBAction func payBtnPressed(_ sender: Any) {
+	}
 	
 }
 
